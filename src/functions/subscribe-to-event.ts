c2011f2c71ed747e9d9c084bf270b5a0b5fb9411
laiza <laiza@wdtecnologia.com.br>
2025-02-24 22:46:40 -0300

    funcionalidades de convites inseridas

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,26 +1,41 @@
+import { eq } from 'drizzle-orm'
 import { db } from '../drizzle/client'
-import { subscriptions } from '../drizzle/schema/subscriptions'
+import { schema } from '../drizzle/schema'
+import { redis } from '../redis/client'
 
 interface SubscribeToEventParams {
   name: string
   email: string
+  invitedBySubscriberId: string | null
 }
 
 export async function subscribeToEvent({
   name,
   email,
+  invitedBySubscriberId,
 }: SubscribeToEventParams) {
-  const result = await db
-    .insert(subscriptions)
+  const results = await db
+    .select()
+    .from(schema.subscriptions)
+    .where(eq(schema.subscriptions.email, email))
+
+  if (results.length > 0) {
+    return { subscriberId: results[0].id }
+  }
+
+  const [{ subscriberId }] = await db
+    .insert(schema.subscriptions)
     .values({
       name,
       email,
     })
-    .returning()
+    .returning({
+      subscriberId: schema.subscriptions.id,
+    })
 
-  const subscriber = result[0]
-
-  return {
-    subscriberId: subscriber.id,
+  if (invitedBySubscriberId) {
+    await redis.zincrby('referral:ranking', 1, invitedBySubscriberId)
   }
+
+  return { subscriberId }
 }
