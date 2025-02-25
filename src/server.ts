ba4041e3db210612b9634b5c06a70ccc9d86f01e
laiza <laiza@wdtecnologia.com.br>
2025-02-24 22:35:00 -0300

    correção na porta

--- src/server.ts
+++ src/server.ts
@@ -77,6 +77,6 @@ app.post('/subscriptions', {
 
 app.register(subscribeToEventRoute)
 
-app.listen({ port: env.PORT || 3000 }).then(() => {
+app.listen({ host: "0.0.0.0", port: env.PORT || 3000 }).then(() => {
   console.log('HTTP server runing!')
 })
